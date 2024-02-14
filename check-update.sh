c4b11e637fdbd6eb1f2ea250bc315935b256b4da
mandian <mandian@users.noreply.github.com>
2024-02-14 23:32:57 +0000

    fix check-update.sh

--- check-update.sh
+++ check-update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl "https://github.com/wxMaxima-developers/wxmaxima/releases" 2>/dev/null |grep "tag/Version-" |sed -e 's,.*tag/Version-,,;s,\".*,,;' |head -n1
+curl "https://github.com/wxMaxima-developers/wxmaxima/releases" 2>/dev/null |grep "tag/Version-" |sed -e 's,.*tag/Version-,,;s,\".*,,;' |sort -V |tail -n1
 
